a0175f4e1f81fd47ae1f93f5e4747f294ff31476
epicrunze <epicrunze@gmail.com>
2021-01-12 17:01:10 -0500

    data intake v1

--- helper_modules/datain.py
+++ helper_modules/datain.py
@@ -1,23 +1,36 @@
+import tkinter as tk
+import pandas as pd
+from . import plot
+
 # code for data intake widget
 class DataIntake(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, datadist=None):
         super().__init__(master)
         self.master = master
+        self.datadist = datadist
         self.pack()
 
         self.create_buttons()
 
+        # parameters governing csv intake
+
     def create_buttons(self):
         self.load_csv = tk.Button(self)
         self.load_csv["text"] = "Select csv to Load"
-        self.load_csv["command"] = self.read_csv
+        self.load_csv["command"] = self.read_csv(datadist=self.datadist)
         self.load_csv.pack(side="left")
 
-    def read_csv(self):
-        # csv or similar file with DEPVAR column, and TIME column
-        filename = tk.filedialog.askopenfilename()
-        
-        # read csv as pandas df, then convert to dictionary
-        self.data = pd.read_csv(filename).to_dict(orient="list")
+    @classmethod
+    def read_csv(cls, datadist=None):
+        def tmp()
+            # csv or similar file with DEPVAR column, and TIME column
+            filename = tk.filedialog.askopenfilename()
+            
+            # read csv as pandas df, then convert to dictionary
+            datadist.data = pd.read_csv(filename).to_dict(orient="list")
 
-        self.plot_csv()
+            plot.Plotter.plot_csv(datadist=datadist)
+        
+        return tmp
+
+    
