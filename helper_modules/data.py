aa85a56ebb6ca7bd167f4d61d757d4a181fdc0ff
epicrunze <epicrunze@gmail.com>
2021-02-23 12:44:38 -0500

    updated docstrings

--- helper_modules/data.py
+++ helper_modules/data.py
@@ -1,6 +1,17 @@
-import numpy as np
 class DataDistributor():
+
     def __init__(self):
+        '''
+            object that is used to share data with the rest of the
+            modules.
+
+            Contains:
+                csv data
+                model object
+                plot variables
+
+                definition for dependent and independent var strings
+        '''
         # Data is a dictionary of lists, with column headers as keys
         self.data = None
         self.model = None
@@ -16,11 +27,20 @@ class DataDistributor():
         self.INDEPVAR = None
 
     def get_vars(self):
-        # function to get column headers that correspond to dependant and independant variables
+        ''' function to get column headers that correspond to
+            dependant and independant variables
+
+            None -> bool
+
+            Returns 1 if csv has proper variables
+            Inplace operation
+
+            updates the depvar and indepvar lists with column headers
+        '''
         if not self.data:
             print("no data Sadge")
             return None
-        
+
         self.DEPVAR = [substr for substr in self.data.keys() if self.DEPVAR_indicator in substr]
         self.INDEPVAR = [substr for substr in self.data.keys() if self.INDEPVAR_indicator in substr]
 
@@ -28,12 +48,12 @@ class DataDistributor():
             self.DEPVAR = None
             print("TOO MANY DEPENDENT VARS")
             return None
-        
+
         if len(self.INDEPVAR) > 1:
             self.INDEPVAR = None
             print("TOO MANY INDEPENDENT VARS")
             return None
-        
+
         self.DEPVAR = self.DEPVAR[0]
         self.INDEPVAR = self.INDEPVAR[0]
 
